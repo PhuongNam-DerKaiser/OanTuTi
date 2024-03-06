8b9baee6fc799204709db8cadb7075a3d2837a74
PhuongNam-DerKaiser <phuongnam.developer@gmail.com>
2024-03-06 22:10:18 +0700

    OanTuTi code by Winform C#

---
+++ OanTuTi/OanTuTi.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace OanTuTi
+{
+    partial class OanTuTi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMayKeo = new System.Windows.Forms.Button();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.btnBanBao = new System.Windows.Forms.Button();
+            this.btnBanBua = new System.Windows.Forms.Button();
+            this.btnBanKeo = new System.Windows.Forms.Button();
+            this.btnMayBua = new System.Windows.Forms.Button();
+            this.btnMayBao = new System.Windows.Forms.Button();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMayKeo
+            // 
+            this.btnMayKeo.Image = global::OanTuTi.Properties.Resources.Keo;
+            this.btnMayKeo.Location = new System.Drawing.Point(412, 371);
+            this.btnMayKeo.Name = "btnMayKeo";
+            this.btnMayKeo.Size = new System.Drawing.Size(165, 128);
+            this.btnMayKeo.TabIndex = 3;
+            this.btnMayKeo.UseVisualStyleBackColor = true;
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.Location = new System.Drawing.Point(347, 9);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(191, 44);
+            this.lbTitle.TabIndex = 4;
+            this.lbTitle.Text = "Oẳn Tù Tì";
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResult.ForeColor = System.Drawing.Color.IndianRed;
+            this.lbResult.Location = new System.Drawing.Point(348, 266);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(144, 41);
+            this.lbResult.TabIndex = 5;
+            this.lbResult.Text = "Kết quả";
+            // 
+            // btnBanBao
+            // 
+            this.btnBanBao.Image = global::OanTuTi.Properties.Resources.Bao;
+            this.btnBanBao.Location = new System.Drawing.Point(624, 79);
+            this.btnBanBao.Name = "btnBanBao";
+            this.btnBanBao.Size = new System.Drawing.Size(165, 128);
+            this.btnBanBao.TabIndex = 2;
+            this.btnBanBao.UseVisualStyleBackColor = true;
+            this.btnBanBao.Click += new System.EventHandler(this.btnBanBao_Click);
+            // 
+            // btnBanBua
+            // 
+            this.btnBanBua.Image = global::OanTuTi.Properties.Resources.Bua;
+            this.btnBanBua.Location = new System.Drawing.Point(412, 79);
+            this.btnBanBua.Name = "btnBanBua";
+            this.btnBanBua.Size = new System.Drawing.Size(165, 128);
+            this.btnBanBua.TabIndex = 1;
+            this.btnBanBua.UseVisualStyleBackColor = true;
+            this.btnBanBua.Click += new System.EventHandler(this.btnBanBua_Click);
+            // 
+            // btnBanKeo
+            // 
+            this.btnBanKeo.Image = global::OanTuTi.Properties.Resources.Keo;
+            this.btnBanKeo.Location = new System.Drawing.Point(183, 79);
+            this.btnBanKeo.Name = "btnBanKeo";
+            this.btnBanKeo.Size = new System.Drawing.Size(165, 128);
+            this.btnBanKeo.TabIndex = 0;
+            this.btnBanKeo.Text = " ";
+            this.btnBanKeo.UseVisualStyleBackColor = true;
+            this.btnBanKeo.Click += new System.EventHandler(this.btnBanKeo_Click);
+            // 
+            // btnMayBua
+            // 
+            this.btnMayBua.Image = global::OanTuTi.Properties.Resources.Bua;
+            this.btnMayBua.Location = new System.Drawing.Point(412, 371);
+            this.btnMayBua.Name = "btnMayBua";
+            this.btnMayBua.Size = new System.Drawing.Size(165, 128);
+            this.btnMayBua.TabIndex = 6;
+            this.btnMayBua.UseVisualStyleBackColor = true;
+            // 
+            // btnMayBao
+            // 
+            this.btnMayBao.Image = global::OanTuTi.Properties.Resources.Bao;
+            this.btnMayBao.Location = new System.Drawing.Point(412, 371);
+            this.btnMayBao.Name = "btnMayBao";
+            this.btnMayBao.Size = new System.Drawing.Size(165, 128);
+            this.btnMayBao.TabIndex = 7;
+            this.btnMayBao.UseVisualStyleBackColor = true;
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.Location = new System.Drawing.Point(27, 116);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(90, 44);
+            this.lb1.TabIndex = 8;
+            this.lb1.Text = "Bạn";
+            // 
+            // lb2
+            // 
+            this.lb2.AutoSize = true;
+            this.lb2.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb2.Location = new System.Drawing.Point(12, 408);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(147, 44);
+            this.lb2.TabIndex = 9;
+            this.lb2.Text = "Đối thủ";
+            // 
+            // OanTuTi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 520);
+            this.Controls.Add(this.lb2);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.btnMayBao);
+            this.Controls.Add(this.btnMayBua);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.lbTitle);
+            this.Controls.Add(this.btnMayKeo);
+            this.Controls.Add(this.btnBanBao);
+            this.Controls.Add(this.btnBanBua);
+            this.Controls.Add(this.btnBanKeo);
+            this.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "OanTuTi";
+            this.Text = "Oẳn Tù Tì";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBanBua;
+        private System.Windows.Forms.Button btnBanBao;
+        private System.Windows.Forms.Button btnMayKeo;
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Button btnBanKeo;
+        private System.Windows.Forms.Button btnMayBua;
+        private System.Windows.Forms.Button btnMayBao;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Label lb2;
+    }
+}
+
---
+++ OanTuTi/OanTuTi.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OanTuTi
+{
+    public partial class OanTuTi : Form
+    {
+        // Một enum định nghĩa các lựa chọn trong trò chơi
+        enum LuaChon { Keo, Bua, Bao };
+
+        // Một mảng chứa các button của đối thủ
+        Button[] opponentButtons;
+
+        // Một biến để lưu lựa chọn của người chơi
+        LuaChon NguoiChoi;
+        public OanTuTi()
+        {
+            InitializeComponent();
+            opponentButtons = new Button[] { btnMayKeo, btnMayBua, btnMayBao };
+        }
+
+        private void btnBanKeo_Click(object sender, EventArgs e)
+        {
+            NguoiChoi = LuaChon.Keo;
+            ChoiGame();
+        }
+
+        private void btnBanBua_Click(object sender, EventArgs e)
+        {
+            NguoiChoi = LuaChon.Bua;
+            ChoiGame();
+        }
+
+        private void btnBanBao_Click(object sender, EventArgs e)
+        {
+            NguoiChoi = LuaChon.Bao;
+            ChoiGame();
+
+        }
+        private void ChoiGame()
+        {
+            // Tạo một lựa chọn ngẫu nhiên cho máy tính
+            Random random = new Random();
+            LuaChon MayTinh = (LuaChon)random.Next(0, 3);
+
+            // Ẩn tất cả các button của đối thủ trước khi hiển thị lựa chọn của máy tính
+            foreach (Button button in opponentButtons)
+            {
+                button.Visible = false;
+            }
+
+            // Hiển thị hình ảnh tương ứng với lựa chọn của máy tính
+            opponentButtons[(int)MayTinh].Visible = true;
+
+            // So sánh lựa chọn của cả hai và hiển thị kết quả
+            if (NguoiChoi == MayTinh)
+            {
+                lbResult.Text = "Hòa nhau!";
+            }
+            else if ((NguoiChoi == LuaChon.Bua && MayTinh == LuaChon.Keo) ||
+                     (NguoiChoi == LuaChon.Bao && MayTinh == LuaChon.Bua) ||
+                     (NguoiChoi == LuaChon.Keo && MayTinh == LuaChon.Bao))
+            {
+                lbResult.Text = "Bạn thắng!";
+            }
+            else
+            {
+                lbResult.Text = "Bạn thua!";
+            }
+        }
+    }
+
+}
